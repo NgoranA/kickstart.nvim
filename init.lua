325e0b4fc8c2c6e764fc796962d8517837a34a70
NgoranA <ngoranaristide4@gmail.com>
2024-11-22 16:58:44 +0100

    updating my configs to match with base

--- init.lua
+++ init.lua
@@ -982,9 +982,13 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+<<<<<<< HEAD
 
   { import = 'custom.plugins' },
 
+=======
+  { import = 'custom.plugins' },
+>>>>>>> 22a6bbd (updated my configs to be inline with base)
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
