e938f32c1179d00e8be833880478e92f6206226d
Artem Dragunov <dragunovartem99@gmail.com>
2024-12-12 18:51:58 +0300

    Use consistent syntax style for { ... } "pseudocode" (#1247)
    ```
    require('gitsigns').setup({ ... })
    ```
    
    This was the first occurrence
    
    It may be nice to have the same style everywhere
    
    Cosmetic change (just to make docs/comments even more perfect)

--- init.lua
+++ init.lua
@@ -657,8 +657,8 @@ require('lazy').setup({
         },
 
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
+          -- cmd = { ... },
+          -- filetypes = { ... },
           -- capabilities = {},
           settings = {
             Lua = {
